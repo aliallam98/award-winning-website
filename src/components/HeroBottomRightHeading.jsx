3c20977618955cf1bce46dd46565218e68db99cc
aliallam98 <allam98.ali@gmail.com>
2024-12-06 15:55:57 +0200

    added  HeroBottomRightHeading and HeroHeadingContainer components .

---
+++ src/components/HeroBottomRightHeading.jsx
@@ -0,0 +1,14 @@
+const HeroBottomRightHeading = () => {
+  return (
+    <>
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 z-50 text-blue-100">
+        G<b>A</b>MING
+      </h1>
+      <h1 className="special-font hero-heading absolute bottom-5 right-5  text-black">
+        G<b>A</b>MING
+      </h1>
+    </>
+  );
+};
+
+export default HeroBottomRightHeading;
